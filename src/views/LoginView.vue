1e3ef0d666e0eebdb090b4d3102601b2f924b47d
MiloVirus <camilovalencia.dev@gmail.com>
2024-12-10 19:52:31 -0500

    EcoDiveCol

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h3>
+            Welcome to The Login Page
+        </h3>
+    </div>
+
+</template>
+<style  lang="scss" scoped>
+
+</style>
